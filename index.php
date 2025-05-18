febfafe0f57aa3eb2718f5793f47b43ad8ef38fe
Fawaz <muhrifatf@gmail.com>
2025-05-18 19:11:08 +0700

    menambahkan struktur dasat html ke index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Stok Barang</title>
+</head>
+<body>
+    <h1>Selamat Datang di Aplikasi Stok Barang</h1>
+    <p>Ini adalah halaman utama proyek PHP Native kita.</p>
+    <p>Kita akan menampilkan daftar barang di sini nanti.</p>
+</body>
+</html>
